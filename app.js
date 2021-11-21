72eb6c6e29e38427d381ebdb8cd1959015445f76
acrighton1 <78578037+acrighton1@users.noreply.github.com>
2021-11-21 14:01:18 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+<div class="container">
+<div class="col-md-4">
+  <ul class="footer-links">
+    <li><a href="default.asp" target="_blank">About</a></li>
+    <li><a href="default.asp" target="_blank">Contact</a></li>
+    <li><a href="default.asp" target="_blank">Privacy Policy</a></li>
+    <li><a href="default.asp" target="_blank">Terms of Use</a></li>
+  </ul>
+  </div>
+  <div class="col-md-4">
+    <div class="footer-copyright text-center">
+      <p>© 2020 Areli Theme. All Rights Reserved.</p>
+</div>
+<div class="col-md-4">
+  <div class="input-group mb-3">
+    <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2">
+    <button class="btn btn-outline-secondary" type="button" id="button-addon2">Button</button>
+  </div>
+</div>
+</div>
